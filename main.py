d573347250015aa713784bfb70d81662037273b9
Alexcostap <102334175+Alexcostap@users.noreply.github.com>
2024-08-05 11:27:41 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-from bibliotecaClasses import *
-# p1 = Pessoa("Alex", 87,24)
-# print(p1.nome,p1.peso,p1.idade)
-# p1.comer("arroz")
-# p1.parouComer()
+import flet as ft
+from models import *
+def main(page: ft.page):
 
-g1 = Gado("nina","branca")
-g1.emitirSom()
-g1.comer()
+    lista_produtos = ft.ListView()
 
 
+
+    def inserir(conteudo):
+        novo_produto = Produto(titulo=produto1.value, preco=preco.value)
+        session.add(novo_produto)
+        session.commit()
+        print('Salvo')
+
+
+
+
+    page.title = 'PURO MILHO'
+    texto_titulo = ft.Text('Olá mundo!!', text_align=ft.TextAlign.LEFT) #Precisa adicionar ao page.add pois ele fica fora dessa função page
+    produto1 = ft.TextField(label='Insira um nome do produto', text_align=ft.TextAlign.LEFT)
+    texto_preco = ft.Text('Preço do Produto')
+    preco = ft.TextField(label='Preço do produto',text_align=ft.TextAlign.LEFT)
+    btn_produto = ft.ElevatedButton('Inserir', on_click=inserir)
+
+    for x in session.query(Produto).all():
+        print(x)
+    largura = page.window.width = 300 #Largura da janela ao abrir
+    altura = page.window.height = 500 #Altura da janela ao abrir
+    page.add(
+        texto_titulo,
+        produto1,
+        texto_preco,
+        preco,
+        btn_produto
+    )
+
+    
+ft.app(target=main)
